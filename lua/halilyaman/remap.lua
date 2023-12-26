0e0892e0520376fd534831bcf8b0b27c115f8e16
halilyaman <halilymn03@gmail.com>
2023-12-26 12:28:46 +0300

    Add switching between buffers

--- lua/halilyaman/remap.lua
+++ lua/halilyaman/remap.lua
@@ -1,3 +1,5 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
+vim.keymap.set("n", "<S-h>", "<cmd>bprev<cr>", { desc = "Previous buffer", silent = true })
+vim.keymap.set("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next buffer", silent = true })
