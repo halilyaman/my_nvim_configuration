97e75f9b0470e07eea90945933a125dcda327c29
halilyaman <halilymn03@gmail.com>
2023-12-29 12:37:33 +0300

    Reformat code

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,30 +1,30 @@
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-	-- see :help lsp-zero-keybindings
-	-- to learn the available actions
-	lsp_zero.default_keymaps({ buffer = bufnr })
+    -- see :help lsp-zero-keybindings
+    -- to learn the available actions
+    lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-	ensure_installed = {
-		"rust_analyzer",
-		"lua_ls",
-	},
-	handlers = {
-		lsp_zero.default_setup,
-	},
+    ensure_installed = {
+        "rust_analyzer",
+        "lua_ls",
+    },
+    handlers = {
+        lsp_zero.default_setup,
+    },
 })
 
 local cmp = require('cmp')
 
 cmp.setup({
-  mapping = cmp.mapping.preset.insert({
-    ['<CR>'] = cmp.mapping.confirm({select = false}),
-  });
-  window = {
-    completion = cmp.config.window.bordered(),
-    documentation = cmp.config.window.bordered(),
-  }
+    mapping = cmp.mapping.preset.insert({
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
+    }),
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    }
 })
