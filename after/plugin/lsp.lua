9637e5b830cd5e288c529469d372142c592063ae
halilyaman <halilymn03@gmail.com>
2024-04-29 12:53:03 +0300

    Separate delete and cut logic

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -5,7 +5,7 @@ lsp_zero.on_attach(function(client, bufnr)
 
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
     vim.keymap.set('n', '<leader>h', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, opts)
+    vim.keymap.set('n', '<leader>di', vim.diagnostic.open_float, opts)
     vim.keymap.set('n', '[d', vim.diagnostic.goto_next, opts)
     vim.keymap.set('n', ']d', vim.diagnostic.goto_prev, opts)
     vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
