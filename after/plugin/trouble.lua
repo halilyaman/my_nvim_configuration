b7e4c6b49cd8b265b8d5a53d7d8dbc62792ebdd1
halilyaman <halilymn03@gmail.com>
2024-05-10 15:23:53 +0300

    Set diagnostic float scope as "buffer"

--- after/plugin/trouble.lua
+++ after/plugin/trouble.lua
@@ -5,4 +5,7 @@ vim.keymap.set("n", "<leader>xx", trouble.toggle)
 vim.diagnostic.config({
     update_in_insert = true,
     severity_sort = true,
+    float = {
+        scope = "buffer",
+    },
 })
